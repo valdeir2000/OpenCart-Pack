74f4223342279da6902c7b80af1bbdda1649eccb
Valdeir Santana <valdeirpsr@hotmail.com.br>
2013-02-22 01:35:13 -0300

    Catalog | Install

---
+++ catalog/language/portuguese-br/payment/paymate.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_title']          = 'Cartão de Crédito/Débito (Paymate)';
+
+$_['text_unable']         = 'Não foi possível localizar ou atualizar a situação do pedido';
+$_['text_declined']       = 'O pagamento foi recusado pelo Paymate';
+$_['text_failed']         = 'Falha na transação no Paymate';
+$_['text_failed_message'] = '<p>Infelizmente houve um erro ao processar sua transação no PayMate.</p><p><b>Atenção: </b>%s</p><p>Por favor, verifique o saldo da conta PayMate antes de tentar novamente efetuar esta compra.</p><p> Se você acredita que esta operação foi concluída com êxito, ou se demonstrou uma dedução na sua conta PayMate, <a href="%s">entre em contato</a> com os detalhes de seu pedido em mãos.</p>';
+$_['text_basket']         = 'Carrinho';
+$_['text_checkout']       = 'Finalizar';
+$_['text_success']        = 'Sucesso';
+?>
---
+++ catalog/language/portuguese-br/payment/worldpay.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']     = 'Obrigado por comprar com %s .... ';
+
+// Text
+$_['text_title']        = 'Cartão de Crédito/Débito (WorldPay)';
+$_['text_response']     = 'Resposta do WorldPay:';
+$_['text_success']      = '... seu pagamento foi recebido com sucesso.';
+$_['text_success_wait'] = '<b><span style="color: #FF0000">Por favor, aguarde...</span></b> enquanto finalizamos o processamento de seu pedido.<br />Se você não for automaticamente redirecionado em 10 segundos, por favor, <a href="%s">clique aqui</a>.';
+$_['text_failure']      = '... seu pagamento foi cancelado!';
+$_['text_failure_wait'] = '<b><span style="color: #FF0000">Por favor, aguarde...</span></b><br />Se você não for automaticamente redirecionado em 10 segundos, por favor, clique <a href="%s">aqui</a>.';									  
+$_['text_pw_mismatch']  = 'A senha de retorno não combina. O pedido requer análise.';
+?>
