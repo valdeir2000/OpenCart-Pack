4b2f2efb0a8fa79fbb14e7825675b7073ad08cee
valdeir2000 <valdeir_webdesign@hotmail.com>
2015-04-27 15:09:51 -0300

    Remove the function "get_template"
    Remove the "get_template" and adds function of the model / template

--- install/controller/step_8.php
+++ install/controller/step_8.php
@@ -86,7 +86,8 @@ class ControllerStep8 extends Controller {
 		$data['module_name'] = reset($this->request->post['modules']);
 
 		/* Captura código do módulo */
-		$code = $this->get_template(reset($this->request->post['modules']));
+		$this->load->model('template');
+		$code = $this->model_template->get_template('feed', reset($this->request->post['modules']));
 
 		/* Carrega libravia */
 		$this->load->library('simple_html_dom');
@@ -111,42 +112,6 @@ class ControllerStep8 extends Controller {
 		/* Template */
 		$this->response->setOutput($this->load->view('extension_template.tpl', $data));
 
-	}
-
-	private function get_template($extension = '') {
-		if (!empty($extension)) {
-
-			/* Define usuário como logado */
-			$this->session->data['user_id'] = '1';
-			$this->session->data['token'] = 'valdeir';
-
-			/* Url do módulo */
-			$url = HTTP_OPENCART . 'admin/index.php?route=feed/' . $extension . '&token=valdeir';
-
-			/* Armazena session */
-			$strCookie = session_name() . '=' . $_COOKIE[ session_name() ] . '; path=/';
-			session_write_close(); 
-
-			/* CURL */
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_HEADER, false);
-			curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $this->request->server['HTTP_USER_AGENT']);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_FORBID_REUSE, false);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-			curl_setopt($ch, CURLOPT_COOKIE, $strCookie);
-			$response = curl_exec($ch);
-			curl_close($ch);
-
-			return $response;
-		} else {
-			return false;
-		}
 	}
 }
 ?>
