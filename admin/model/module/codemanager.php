fe2b3673e6da8b2d940c46430a950ddb3b817687
valdeir2000 <valdeir_webdesign@hotmail.com>
2015-04-28 06:21:37 -0300

    Update Code Manager

---
+++ admin/model/module/codemanager.php
@@ -0,0 +1,39 @@
+<?php 
+class ModelModuleCodeManager extends Model {
+
+  	public function getSetting($group, $store_id = 0) {
+	    $data = array(); 
+	    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `group` = '" . $this->db->escape($group) . "'");
+	    foreach ($query->rows as $result) {
+	      if (!$result['serialized']) {
+	        $data[$result['key']] = $result['value'];
+	      } else {
+	        $data[$result['key']] = unserialize($result['value']);
+	      }
+	    } 
+	    return $data;
+	}
+  
+  	public function editSetting($group, $data, $store_id = 0) {
+	    $this->db->query("DELETE FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `group` = '" . $this->db->escape($group) . "'");
+	    foreach ($data as $key => $value) {
+	      if (!is_array($value)) {
+	        $this->db->query("INSERT INTO " . DB_PREFIX . "setting SET store_id = '" . (int)$store_id . "', `group` = '" . $this->db->escape($group) . "', `key` = '" . $this->db->escape($key) . "', `value` = '" . $this->db->escape($value) . "'");
+	      } else {
+	        $this->db->query("INSERT INTO " . DB_PREFIX . "setting SET store_id = '" . (int)$store_id . "', `group` = '" . $this->db->escape($group) . "', `key` = '" . $this->db->escape($key) . "', `value` = '" . $this->db->escape(serialize($value)) . "', serialized = '1'");
+	      }
+	    }
+	}
+	
+  	public function install() {
+		$this->db->query("UPDATE `" . DB_PREFIX . "modification` SET status=1 WHERE `name` LIKE'%CodeManager by iSenseLabs%'");
+		$modifications = $this->load->controller('extension/modification/refresh');
+  	} 
+  
+  	public function uninstall() {
+		$this->db->query("UPDATE `" . DB_PREFIX . "modification` SET status=0 WHERE `name` LIKE'%CodeManager by iSenseLabs%'");
+		$modifications = $this->load->controller('extension/modification/refresh');
+  	}
+	
+  }
+?>
